23526c559b94995b6157b4b78d1f364c1e81d054
aragami3070 <smirnov17612@gmail.com>
2025-09-15 13:37:32 +0400

    Feat(entities): add Edge struct with impl default trait

---
+++ src/graph/entities.rs
@@ -0,0 +1,21 @@
+type Index = u32;
+type Weight = u32;
+
+pub struct Edge<T> {
+    pub number: Index,
+    pub weight: Weight,
+    pub value: T,
+}
+
+impl<T> Default for Edge<T>
+where
+    T: Default,
+{
+    fn default() -> Self {
+        Edge::<T> {
+            number: 0,
+            weight: 0,
+            value: T::default(),
+        }
+    }
+}
