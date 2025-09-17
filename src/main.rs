3642402fbcea44cea494943921b5715fc69f154c
aragami3070 <smirnov17612@gmail.com>
2025-09-17 12:56:57 +0400

    Chore(graph): rename entities module to core

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 mod graph;
+use graph::core;
 fn main() {
+	let edge = core::Edge::<u32>::new(1, 100, 20);
+    let adjacency = core::Adjacency::<u32>::new(edge);
+	let adjacency_list = core::AdjacencyList::<u32>::new(1, adjacency);
+	println!("{adjacency_list}")
 }
