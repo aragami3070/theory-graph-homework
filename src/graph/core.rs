55c382566971e4cb8f6e234774df1c9c16b8e4cd
aragami3070 <smirnov17612@gmail.com>
2025-09-22 12:26:58 +0400

    Feat(core): add Node struct and Clone derive

--- src/graph/core.rs
+++ src/graph/core.rs
@@ -6,40 +6,78 @@ use std::{
 type Index = u32;
 type Weight = u32;
 
-// Edge part
+// Node part
 
-pub struct Edge<T> {
+#[derive(Clone)]
+pub struct Node<T> {
     pub number: Index,
-    pub weight: Weight,
     pub value: T,
 }
 
-impl<T> PartialEq for Edge<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.number == other.number
+impl<T> Default for Node<T>
+where
+    T: Default,
+    T: Clone,
+{
+    fn default() -> Self {
+        Node::<T> {
+            number: 0,
+            value: T::default(),
+        }
     }
-    fn ne(&self, other: &Self) -> bool {
-        self.number != other.number
+}
+impl<T> Node<T> {
+    pub fn new(index: Index, value: T) -> Self {
+        Self {
+            number: index,
+            value,
+        }
     }
 }
 
-impl<T> Eq for Edge<T> {}
+// Edge part
 
-impl<T> Hash for Edge<T> {
+#[derive(Clone)]
+pub struct Edge<T>
+where
+    T: Clone,
+{
+    pub node: Node<T>,
+    pub weight: Weight,
+}
+
+impl<T> PartialEq for Edge<T>
+where
+    T: Clone,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.node.number == other.node.number
+    }
+    fn ne(&self, other: &Self) -> bool {
+        self.node.number != other.node.number
+    }
+}
+
+impl<T> Eq for Edge<T> where T: Clone {}
+
+impl<T> Hash for Edge<T>
+where
+    T: Clone,
+{
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.number.hash(state);
+        self.node.number.hash(state);
     }
 }
 
 impl<T> Default for Edge<T>
 where
     T: Default,
+    T: Clone,
 {
     fn default() -> Self {
         Edge::<T> {
-            number: 0,
+            node: Node::default(),
             weight: 0,
-            value: T::default(),
         }
     }
 }
@@ -47,34 +85,43 @@ where
 impl<T> Display for Edge<T>
 where
     T: Display,
+    T: Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "  Edge: {{\n    Number: {},\n    Weight: {},\n    Value: {}\n  }}",
-            self.number, self.weight, self.value
+            self.node.number, self.weight, self.node.value
         )
     }
 }
 
-impl<T> Edge<T> {
+impl<T> Edge<T>
+where
+    T: Clone,
+{
     /// Creates a new [`Edge<T>`].
-    pub fn new(number: Index, weight: Weight, value: T) -> Self {
+    pub fn new(number: &Index, weight: Weight, value: &T) -> Self {
         Self {
-            number,
             weight,
-            value,
+            node: Node::new(*number, value.clone()),
         }
     }
 }
 
 // Adjacency part
 
-pub struct Adjacency<T> {
+pub struct Adjacency<T>
+where
+    T: Clone,
+{
     edges: HashSet<Edge<T>>,
 }
 
-impl<T> Default for Adjacency<T> {
+impl<T> Default for Adjacency<T>
+where
+    T: Clone,
+{
     fn default() -> Self {
         Self {
             edges: HashSet::new(),
@@ -85,6 +132,7 @@ impl<T> Default for Adjacency<T> {
 impl<T> Display for Adjacency<T>
 where
     T: Display,
+    T: Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut print_format = String::from("[\n");
@@ -96,7 +144,10 @@ where
     }
 }
 
-impl<T> Adjacency<T> {
+impl<T> Adjacency<T>
+where
+    T: Clone,
+{
     /// Creates a new [`Adjacency<T>`]
     pub fn new(edge: Edge<T>) -> Self {
         let mut new_edges = HashSet::new();
@@ -105,18 +156,25 @@ impl<T> Adjacency<T> {
     }
 
     /// Add in end new [`Edge<T>`]
-    pub fn push(&mut self, edge: Edge<T>) {
+    fn push(&mut self, edge: Edge<T>) {
         self.edges.insert(edge);
     }
 }
 
-pub struct AdjacencyList<T> {
+// AdjacencyList part
+
+pub struct AdjacencyList<T>
+where
+    T: Clone,
+{
     edges: HashMap<Index, Adjacency<T>>,
+    is_directed: bool,
 }
 
 impl<T> Display for AdjacencyList<T>
 where
     T: Display,
+    T: Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut print_format = String::new();
@@ -127,24 +185,53 @@ where
     }
 }
 
-impl<T> Default for AdjacencyList<T> {
+impl<T> Default for AdjacencyList<T>
+where
+    T: Clone,
+{
     fn default() -> Self {
         Self {
             edges: HashMap::new(),
+            is_directed: false,
         }
     }
 }
 
-impl<T> AdjacencyList<T> {
-    /// Creates a new [`AdjacencyList<T>`]
-    pub fn new(index_node: Index, edge_adjacency: Adjacency<T>) -> Self {
+impl<T> AdjacencyList<T>
+where
+    T: Clone,
+{
+    /// Creates a new not directed [`AdjacencyList<T>`]
+    pub fn new(index_node: Index, edge_adjacency: Adjacency<T>, is_directed: bool) -> Self {
         let mut new_edges: HashMap<Index, Adjacency<T>> = HashMap::new();
         new_edges.insert(index_node, edge_adjacency);
 
-        Self { edges: new_edges }
+        Self {
+            edges: new_edges,
+            is_directed: is_directed,
+        }
     }
 
     pub fn add_node(&mut self, index_node: Index) {
         self.edges.insert(index_node, Adjacency::default());
+    }
+
+    pub fn add_edge(&mut self, node: Node<T>, new_edge: Edge<T>) {
+        if let Some(edges) = self.edges.get_mut(&node.number) {
+            edges.push(new_edge.clone());
+        } else {
+            self.edges
+                .insert(node.number, Adjacency::new(new_edge.clone()));
+        }
+
+        if !self.is_directed {
+            let duplicate_edge = Edge::new(&node.number, new_edge.weight, &node.value);
+            if let Some(edges) = self.edges.get_mut(&new_edge.node.number) {
+                edges.push(duplicate_edge);
+            } else {
+                self.edges
+                    .insert(new_edge.node.number, Adjacency::new(duplicate_edge));
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 mod graph;
 use graph::core;
 fn main() {
-	let edge = core::Edge::<u32>::new(1, 100, 20);
-    let adjacency = core::Adjacency::<u32>::new(edge);
-	let adjacency_list = core::AdjacencyList::<u32>::new(1, adjacency);
-	println!("{adjacency_list}")
+    let node_a = core::Node::<u32>::new(1, 20);
+    let node_b = core::Node::<u32>::new(2, 23);
+    let node_c = core::Node::<u32>::new(3, 23);
+
+    let edge = core::Edge::<u32>::new(&node_a.number, 140, &node_a.value);
+    let edge2 = core::Edge::<u32>::new(&node_b.number, 109, &node_b.value);
+    let edge3 = core::Edge::<u32>::new(&node_c.number, 10, &node_c.value);
+
+    let adjacency = core::Adjacency::<u32>::new(edge2);
+    let mut adjacency_list = core::AdjacencyList::<u32>::new(1, adjacency, true);
+
+    adjacency_list.add_node(node_b.number);
+    adjacency_list.add_edge(node_b.clone(), edge);
+    adjacency_list.add_node(node_c.number);
+    adjacency_list.add_edge(node_b, edge3);
+    println!("{adjacency_list}")
 }
