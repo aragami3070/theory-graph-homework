46a29d64be32a97c58ee946401833e607a34f8b6
aragami3070 <smirnov17612@gmail.com>
2025-10-05 22:40:07 +0400

    Feat(cli): init cli mod with cli_interface method

---
+++ src/graph/cli.rs
@@ -0,0 +1,93 @@
+use std::error::Error;
+
+use crate::graph::core::{Adjacency, AdjacencyList, Edge, Node};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+pub fn cli_interface() -> Result<()> {
+    println!("Создать ориентированный граф?");
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let is_directed: bool = input.trim().parse()?;
+    let mut adjacency_list: AdjacencyList<u32> =
+        AdjacencyList::new(None, Adjacency::default(), is_directed);
+    loop {
+        println!("1. Добавить вершину");
+        println!("2. Добавить ребро");
+        println!("3. Удалить вершину");
+        println!("4. Удалить ребро");
+        println!("5. Сохранить в файл");
+        println!("6. Создать из файла");
+        println!("7 и больше. Выйти");
+
+        input.clear();
+        std::io::stdin().read_line(&mut input)?;
+
+        let choice: u8 = input.trim().parse()?;
+        match choice {
+            1 => {
+                println!("Введите номер вершины:");
+                input.clear();
+                std::io::stdin().read_line(&mut input)?;
+                let node_number: u32 = input.trim().parse()?;
+                adjacency_list.add_node(node_number);
+                println!("{adjacency_list}")
+            }
+            2 => {
+                println!("Введите номер вершины в которую будет идти ребро:");
+
+                input.clear();
+                std::io::stdin().read_line(&mut input)?;
+                let edge_node_number: u32 = input.trim().parse()?;
+
+                println!("Введите ее значение:");
+
+                input.clear();
+                std::io::stdin().read_line(&mut input)?;
+                let edge_node_value: u32 = input.trim().parse()?;
+
+                println!("Введите вес ребра:");
+
+                input.clear();
+                std::io::stdin().read_line(&mut input)?;
+                let edge_node_weight: u32 = input.trim().parse()?;
+
+                let new_edge = Edge::new(&edge_node_number, edge_node_weight, &edge_node_value);
+
+                println!("Введите номер вершины из которой будет выходить ребро:");
+                input.clear();
+                std::io::stdin().read_line(&mut input)?;
+                let node_number: u32 = input.trim().parse()?;
+
+                if adjacency_list.get_is_directed() {
+                    adjacency_list.add_edge(&Node::new(node_number, 0), &new_edge);
+                } else {
+                    println!("Введите ее значение:");
+                    input.clear();
+                    std::io::stdin().read_line(&mut input)?;
+                    let node_value: u32 = input.trim().parse()?;
+
+                    adjacency_list.add_edge(&Node::new(node_number, node_value), &new_edge);
+                }
+                println!("{adjacency_list}")
+            }
+            3 => {
+                todo!()
+            }
+            4 => {
+                todo!()
+            }
+            5 => {
+                todo!()
+            }
+            6 => {
+                todo!()
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,11 @@
+use std:: process;
+
+use crate::graph::cli;
+
 mod graph;
-use std::process;
-
-use graph::core;
 fn main() {
-    let node_a = core::Node::<u32>::new(1, 20);
-    let node_b = core::Node::<u32>::new(2, 23);
-    let node_c = core::Node::<u32>::new(3, 32);
-    let node_d = core::Node::<u32>::new(4, 400);
-    let node_e = core::Node::<u32>::new(5, 500);
-
-    let edge = core::Edge::<u32>::new(&node_a.number, 140, &node_a.value);
-    let edge2 = core::Edge::<u32>::new(&node_b.number, 109, &node_b.value);
-    let edge3 = core::Edge::<u32>::new(&node_c.number, 10, &node_c.value);
-    let edge4 = core::Edge::<u32>::new(&node_d.number, 14, &node_d.value);
-    let edge5 = core::Edge::<u32>::new(&node_e.number, 15, &node_e.value);
-
-    let adjacency = core::Adjacency::<u32>::new(edge2);
-    let mut adjacency_list = core::AdjacencyList::<u32>::new(&node_a, adjacency, false);
-
-    println!("Добавление вершин");
-    adjacency_list.add_node(node_b.number);
-    adjacency_list.add_node(node_c.number);
-    adjacency_list.add_node(node_d.number);
-    println!("{adjacency_list}");
-
-    println!("Добавление ребер");
-    adjacency_list.add_edge(&node_b, &edge);
-    adjacency_list.add_edge(&node_b, &edge3);
-
-    adjacency_list.add_edge(&node_c, &edge4);
-    adjacency_list.add_edge(&node_c, &edge5);
-
-    adjacency_list.add_edge(&node_d, &edge3);
-    println!("{adjacency_list}");
-
-    println!("Удаление ребер");
-    adjacency_list.delete_edge(&node_b, &edge3.node.number);
-    adjacency_list.delete_edge(&node_b, &edge3.node.number);
-    println!("{adjacency_list}");
-
-    println!("Удаление вершин и ребер");
-    adjacency_list.delete_node(&node_c);
-    println!("{adjacency_list}");
-
-	println!("Запись в файл");
-    if let Err(err) = adjacency_list.write_in_file("assets/output.json") {
-        eprintln!("Error: {err}");
-        process::exit(1);
-    }
-	println!("Успешно");
-
-	println!("Создание из файла");
-    adjacency_list = match adjacency_list.new_from_file("assets/output.json") {
-        Ok(list) => list,
-        Err(err) => {
-            eprintln!("Error: {err}");
-            process::exit(1);
-        }
-    };
-
-	println!("{adjacency_list}")
+	if let Err(err) = cli::cli_interface() {
+		eprintln!("Error: {err}");
+		process::exit(1);
+	}
 }
