2b9f63ab1ef0b11a77e4ba12af5cadd048224daf
aragami3070 <smirnov17612@gmail.com>
2025-10-09 12:34:46 +0400

    Feat(tasks): add task_1.rs

---
+++ src/tasks/task_1.rs
@@ -0,0 +1,17 @@
+use crate::graph::core::Graph;
+
+pub fn task_1_4<T: Clone>(graph: &Graph<T>, node_index: &u32) -> u32 {
+    let mut half_step: u32 = 0;
+    for (node_ind, adjacency) in graph {
+        if node_ind == node_index {
+            continue;
+        }
+        for edg in adjacency {
+            if edg.node.number == *node_index {
+                half_step += 1;
+				break;
+            }
+        }
+    }
+	half_step
+}
