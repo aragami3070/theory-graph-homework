396b291efabb4cff1cb9a19726c53422f407f9da
aragami3070 <smirnov17612@gmail.com>
2025-10-13 13:36:00 +0400

    Chore(tasks): add comments

--- src/tasks/task_1.rs
+++ src/tasks/task_1.rs
@@ -1,11 +1,12 @@
 use crate::graph::core::Graph;
 
+/// Получить полустепень захода данной вершины орграфа
 pub fn task_1_4<T: Clone>(graph: &Graph<T>, node_index: &u32) -> u32 {
+    // Полустепень вершины
     let mut half_step: u32 = 0;
-    for (node_ind, adjacency) in graph {
-        if node_ind == node_index {
-            continue;
-        }
+    // Проходимся по всем вершинам и считаем сколько раз встречается
+    // эта вершина в ребрах
+    for (_, adjacency) in graph {
         for edg in adjacency {
             if edg.node.number == *node_index {
                 half_step += 1;
--- src/tasks/task_2.rs
+++ src/tasks/task_2.rs
@@ -1,6 +1,8 @@
 use crate::graph::core::Graph;
 
+/// Получить степень каждой вершины орграфа
 pub fn task_2_5<T: Clone>(graph: &Graph<T>) -> Vec<(u32, u32)> {
+    // Вектор пар (вершина, степень вершины)
     let mut result: Vec<(u32, u32)> = Vec::new();
     for (&node_index, adjacency) in graph {
         result.push((node_index, adjacency.len().try_into().unwrap()));
