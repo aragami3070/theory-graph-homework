cd09b73f03dd7f4381cc3d7d49ea508792734f4c
aragami3070 <smirnov17612@gmail.com>
2025-11-09 22:57:20 +0400

    Feat(task_5): add fn to check cycle in graph

--- src/tasks/task_5.rs
+++ src/tasks/task_5.rs
@@ -1,25 +1,55 @@
-use std::{error::Error, fmt::Debug};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    fmt::Debug,
+};
 
 use serde::{Serialize, de::DeserializeOwned};
 
-use crate::graph::core::{Graph, GraphError, GraphKindError, GraphType};
+use crate::graph::core::{ColorNode, Graph, GraphError, GraphKindError, GraphType};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 
-pub fn task_5_18<T: Clone + DeserializeOwned + Debug + Serialize>(
+fn graph_have_cycle<T: Clone + DeserializeOwned + Debug + Serialize>(
     graph: &Graph<T>,
-) -> Result<GraphType, Box<dyn Error>> {
-	if !graph.get_is_directed() {
+    start: &u32,
+    visited: &mut HashMap<u32, ColorNode>,
+) -> Result<bool> {
+    if let Some(color) = visited.get_mut(start) {
+        // Если мы заходим в серую вершину, то нашли цикл
+        if *color == ColorNode::Gray {
+            return Ok(true);
+        }
+
+        // Перекрашиваем вершину в серую
+        *color = ColorNode::Gray;
+    } else {
         return Err(Box::new(GraphError::new(
-            GraphKindError::GraphMustBeDirected,
-            "по условию должен быть орграф",
+            GraphKindError::NodeNotFound,
+            "Граф пустой",
         )));
-	}
+    };
 
-	let mut count = graph.len();
-	for (ind, adjacency) in graph {
-	    count -= adjacency.len();
+    if let Some(adjacency) = graph.get_adjacency(start) {
+        // Проходимся по всем смежным вершинам
+        for neighbor in adjacency {
+            // Если
+            if let Some(color) = visited.get(&neighbor.node.number)
+                && *color == ColorNode::White
+            {
+                if graph_have_cycle(graph, &neighbor.node.number, visited)? {
+                    return Ok(true);
+                }
+            }
+        }
+    }
 
-	}
+    if let Some(node_index) = visited.get_mut(start) {
+        // Перекрашиваем вершину в черную
+        *node_index = ColorNode::Black
+    }
 
-    Ok(GraphType::Default)
+    Ok(false)
 }
+
