3200f4cc77a4628469cb06dd67243bd00740dc99
aragami3070 <smirnov17612@gmail.com>
2025-11-10 14:57:13 +0400

    Feat(task_6): add bfs for task_6_4

--- src/tasks/task_6.rs
+++ src/tasks/task_6.rs
@@ -1,3 +1,37 @@
-pub fn task_6_4() {
-    
+use std::{
+    collections::{HashSet, VecDeque},
+    error::Error,
+    fmt::Debug,
+};
+
+use serde::{Serialize, de::DeserializeOwned};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+use crate::graph::core::{Graph, GraphError, GraphKindError};
+
+fn bfs<T: Clone + DeserializeOwned + Debug + Serialize + Default>(
+    graph: &Graph<T>,
+    start: &u32,
+    visited: &mut HashSet<u32>,
+) {
+    // Создали очередь со стартовой вершиной
+    let mut queue: VecDeque<u32> = VecDeque::new();
+    queue.push_back(*start);
+    visited.insert(*start);
+
+    // Пока в очереди есть вершины
+    while let Some(current) = queue.pop_front() {
+        if let Some(adjacency) = graph.get_adjacency(&current) {
+            for neighbor in adjacency {
+                let neighbor_index = neighbor.node.number;
+                // Если смежная вершина не посещена, то добавляем в очередь
+                // и отмечаем как посещенную
+                if !visited.contains(&neighbor_index) {
+                    visited.insert(neighbor_index);
+                    queue.push_back(neighbor_index);
+                }
+            }
+        }
+    }
 }
+
